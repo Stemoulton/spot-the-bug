2702ec0f4da342d858b2394c2ac73bd43681749a
Stephen Moulton <moultons01@gmail.com>
2022-08-10 15:07:21 +0100

    Typescript bug update

--- main.ts
+++ main.ts
@@ -6,12 +6,18 @@ interface ZooAnimal {
   diet: string
 }
 
+// BUG - Missing async keyword, required for using await below
+//const fetchZooAnimals = async (numberToFetch: number) => {
 const fetchZooAnimals = (numberToFetch: number) => { 
   const apiUrl = `https://zoo-animal-api.herokuapp.com/animals/rand/${numberToFetch}`
 
+  // BUG - Using the wrong syntax for declaring the type
+  //const { data } = await axios.get<ZooAnimal[]>(apiUrl) 
   const { data } = await axios.get[ZooAnimal[]](apiUrl) 
 
   const zooAnimals = data
+    // BUG - using a .forEach() rather than a .map(), .forEach() doesn't return anything
+    //.map((animal) => ({ Name: animal.name, Diet: animal.diet }))
     .forEach((animal) => ({ Name: animal.name, Diet: animal.diet }))
     .sort((first, second) => first.Name.localeCompare(second))
 
